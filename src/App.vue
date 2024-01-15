c9b06558c86a10395cc04075fd65497669169099
DamianoL <damianolunardo@hotmail.it>
2024-01-15 17:42:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import ProjectIndex from './pages/project/Index.vue';
+export default {
+  components: {
+    ProjectIndex,
+  },
+  data() {
+    return {
+      projects: []
+    }
+  }
+}
+</script>
+
+<template>
+
+<ProjectIndex />
+
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+
+</style>
---
+++ src/pages/project/Index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div class="container">
+        <h1>Project</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="grid">
+<div class="card project-card" v-for="project in projects" :key="project.id">
+<h3>{{ project.title }}</h3>
+</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+   data() {
+    return {
+        projects: []
+    }
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
