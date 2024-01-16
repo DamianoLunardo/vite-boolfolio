be9449850b9c67da7e5a12faed0497f24c201721
DamianoL <damianolunardo@hotmail.it>
2024-01-16 18:21:42 +0100

    Add - dynamic route for detail projects & error 404 page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import ProjectsIndex from './pages/project/Index.vue'
+import ProjectsShow from './pages/project/Show.vue'
 import Contacts from './components/Contacts.vue'
+import NotFound from './pages/404.vue'
+
+
 const router = createRouter({
     history:createWebHistory(),
         routes:[
@@ -16,10 +20,21 @@ const router = createRouter({
                 component: ProjectsIndex,
             },
             {
+                path: '/projects/:slug',
+                name: 'projects.show',
+                props: true,
+                component: ProjectsShow,
+            },
+            {
                 path: '/contacts',
                 name: 'contacts',
                 component: Contacts,
             },
+            {
+                path: '/:pathMatch(.*)*',
+                name: 'not-found',
+                component: NotFound,
+            }
         ],
 })
 
